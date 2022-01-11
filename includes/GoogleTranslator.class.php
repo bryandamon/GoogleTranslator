be7cbd58df017d1651d53069d9af5123ba23d6d4
Bryan Hilderbrand <bryandamon@gmail.com>
2022-01-10 18:30:37 -0800

    Update GoogleTranslator.class.php

--- includes/GoogleTranslator.class.php
+++ includes/GoogleTranslator.class.php
@@ -9,17 +9,33 @@ if (!defined('MEDIAWIKI')) die();
  */
 class GoogleTranslator {
         static function GoogleTranslatorInSidebar( $skin, &$bar ) {
-                global $wgGoogleTranslatorOriginal,$wgGoogleTranslatorLanguages;
 
-                $bar['Translate'] = "<div id=\"google_translate_element\"></div><script>
-                                        function googleTranslateElementInit() {
-                                          new google.translate.TranslateElement({
-                                            pageLanguage: '".$wgGoogleTranslatorOriginal."',
-                                            includedLanguages: '".$wgGoogleTranslatorLanguages."'
-                                          }, 'google_translate_element');
-                                        }
-                                        </script><script src=\"//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit\"></script>";
-                return $bar;
+                        $bar[ 'googletranslator' ] =  array(
+                                array(
+                                        'text'   => '',
+                                        'href'   => '#',
+                                        'id'     => 'google_translate_element',
+                                        'active' => ''
+                                ),
+                        );
+
+
                 return true;
         }
+        static function OnSkinAfterContent (&$data, $skin) {
+                global $wgGoogleTranslatorOriginal,$wgGoogleTranslatorLanguages;
+        $data .=
+                        "
+        <script>
+                        function googleTranslateElementInit() {
+                          new google.translate.TranslateElement({
+                                pageLanguage: '".$wgGoogleTranslatorOriginal."',
+                                includedLanguages: '".$wgGoogleTranslatorLanguages."',
+                                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+                          }, 'google_translate_element');
+                        }
+        </script>
+        <script src=\"//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit\"></script>".
+                        '';
+    }
 }
