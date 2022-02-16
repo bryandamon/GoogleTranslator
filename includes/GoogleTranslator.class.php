8b6a02d958d092e933c17d14e4fd7f36a93a4d0c
Bryan Hilderbrand <bryandamon@gmail.com>
2022-02-15 17:16:55 -0800

    Update GoogleTranslator.class.php
    Removed renaming "Select Language" to "Language".

--- includes/GoogleTranslator.class.php
+++ includes/GoogleTranslator.class.php
@@ -50,18 +50,6 @@ class GoogleTranslator {
                         }, 'google_translate_element');
                 }
         </script>
-        <script src=\"//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit\"></script>
-        <script>
-                $(document).ready(function(){
-                  $('#google_translate_element').bind('DOMNodeInserted', function(event) {
-                    $('.goog-te-menu-value span:first').html('Language');
-                    $('.goog-te-menu-frame.skiptranslate').load(function(){
-                      setTimeout(function(){
-                        $('.goog-te-menu-frame.skiptranslate').contents().find('.goog-te-menu2-item-selected .text').html('Language');
-                      }, 100);
-                    });
-                  });
-                });
-        </script>";
+        <script src=\"//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit\"></script>";
     }
 }
