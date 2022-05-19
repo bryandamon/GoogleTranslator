9bb750332be5fd0328d4c9d7dccd4eeb1c9524e2
Bryan Hilderbrand <bryandamon@gmail.com>
2022-05-18 17:01:27 -0700

    Fix for interference with Source Editor

--- includes/GoogleTranslator.class.php
+++ includes/GoogleTranslator.class.php
@@ -14,42 +14,41 @@ if (!defined('MEDIAWIKI')) die();
 #$wgGoogleTranslatorLanguages  = 'fr,de';        // Languages included in the translating box
 
 class GoogleTranslator {
-        static function GoogleTranslatorInSidebar( $skin, &$bar ) {
-                global $wgOut;
+	static function GoogleTranslatorInSidebar( $skin, &$bar ) {
+		global $wgOut;
 
-        $wgOut->addModules( 'ext.GoogleTranslator' );
+		$wgOut->addModules( 'ext.GoogleTranslator' );
 
-                        $bar[ 'googletranslator' ] =  array(
-                                array(
-                                        'text'   => '',
-                                        'href'   => '#',
-                                        'id'     => 'google_translate_element',
-                                        'active' => ''
-                                ),
-                        );
+		$bar[ 'googletranslator' ] =  [
+			[
+				'text'   => '',
+				'href'   => '#',
+				'id'     => 'google_translate_element',
+				'active' => '',
+			],
+		];
 
+		return true;
+	}
 
-                return true;
-        }
-        static function OnSkinAfterContent (&$data, $skin) {
-                global $wgGoogleTranslatorOriginal,$wgGoogleTranslatorLanguages;
+	static function OnSkinAfterContent ( &$data, $skin ) {
+		global $wgGoogleTranslatorOriginal, $wgGoogleTranslatorLanguages;
 
-        # Default values for the variables.
-        #$wgGoogleTranslatorOriginal  = $wgLanguageCode; // Original languages of the page that needs translation
-        #$wgGoogleTranslatorLanguages  = 'fr,de';        // Languages included in the translating box
+		# Default values for the variables.
+		#$wgGoogleTranslatorOriginal  = $wgLanguageCode; // Original languages of the page that needs translation
+		#$wgGoogleTranslatorLanguages  = 'fr,de';        // Languages included in the translating box
 
-
-        $data = "
-        <script src='https://code.jquery.com/jquery-2.1.4.js'></script>
-        <script>
-                function googleTranslateElementInit() {
-                        new google.translate.TranslateElement({
-                                pageLanguage: '".$wgGoogleTranslatorOriginal."',
-                                includedLanguages: '".$wgGoogleTranslatorLanguages."',
-                                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-                        }, 'google_translate_element');
-                }
-        </script>
-        <script src=\"//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit\"></script>";
-    }
+		$data = <<<EOD
+<script>
+	function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+                pageLanguage: '$wgGoogleTranslatorOriginal',
+                includedLanguages: '$wgGoogleTranslatorLanguages',
+                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        }, 'google_translate_element');
+	}
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+EOD;
+	}
 }
